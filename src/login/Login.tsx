9f2ac7d138db52e1ab65da3f073189f801c12f63
Misha <63599737+MichailNonProgramer@users.noreply.github.com>
2022-03-18 15:51:15 +0500

    Add css login page

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import Logo from "../shared/Logo";
 import {Gapped, Input, PasswordInput} from "@skbkontur/react-ui";
+import './Login.css'
 
 const Login = () => {
     const [userName, setUserName] = useState();
@@ -8,25 +9,31 @@ const Login = () => {
     const [errors, setErrors] = useState({name:'', password: ''});
 
     return (<div className="login">
-        <Logo className="logo" width={250} height={250} />
+        <div className="logo-login">
+        <Logo className="logo_dess" width={200} height={200} />
+        </div>
         <Gapped gap={30} vertical>
-            <div className="userName">Эл.Почта или имя пользователя
-            <Input>Логин</Input>
+            <div className="userName">
+                <p>Эл.Почта или имя пользователя</p>
+                <Input width={'100%'}>Логин</Input>
             </div>
-            <div className="password"> Пароль
-                <PasswordInput>password</PasswordInput>
+            <div className="password">
+                <p>Пароль</p>
+                <PasswordInput detectCapsLock width={320}>password</PasswordInput>
             </div>
-            <div>
-                <label>
-                    <a className="resetPassword" href=''>Забыли пароль?</a>
-                </label>
-                <label>
-                    <button>Войти</button>
-                </label>
-            </div>
-            <div>
-                <a>Нет аккаунта?</a><br/>
-                <a href="">Зарегистрироваться</a>
+            <Gapped gap={5}  className="password_handler">
+                <div className="label_helpers_source">
+                    <a className="reset_password" href=''>Забыли пароль?</a><br/>
+                    <input id = "checked" type="checkbox" className="checked_remember_password"/>
+                    <label className='label_checked' htmlFor="checked">Запомнить меня</label>
+                </div>
+                <div className="label_login_button">
+                    <button className="login__button">Войти</button>
+                </div>
+            </Gapped>
+            <div className="registration">
+                <a className="not_acc">Нет аккаунта?</a>
+                <a className="registration_button" href="">Зарегистрироваться</a>
             </div>
         </Gapped>
     </div>)
