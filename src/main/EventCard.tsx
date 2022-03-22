5df7af1b65546474053573670d67dfb9b57343cb
Banan-dot <semke.danya@mail.ru>
2022-03-23 00:00:32 +0500

    Add submenu on profile hover

--- src/main/EventCard.tsx
+++ src/main/EventCard.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import EventInformation from "../shared/event";
-import "./EventCard.css"
+import EventInformation from '../shared/event';
+import './EventCard.css';
 type EventCardProps = {
-    eventInfo: EventInformation
-}
+    eventInfo: EventInformation;
+};
 
 export default function EventCard(event: EventCardProps) {
     return (
         <div className="event-card">
             <h2>{event.eventInfo.getAllInfo().name}</h2>
-            <div className="description">{event.eventInfo.getAllInfo().description}</div>
-            <div className="event-card-data">Дата начала: {event.eventInfo.getAllInfo().dateOfBeginning?.toDateString()}</div>
-            <div className="event-card-data">Дата конца: {event.eventInfo.getAllInfo().dateOfEnding?.toDateString()}</div>
+            <div className="description">{event.eventInfo.getDescription()}</div>
+            <div className="event-card-data">
+                Дата начала: {event.eventInfo.getDateOfBeginning()?.toLocaleDateString()}
+            </div>
+            <div className="event-card-data">Дата конца: {event.eventInfo.getDateOfEnding()?.toLocaleDateString()}</div>
         </div>
     );
 }
