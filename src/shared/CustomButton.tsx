e5ebcb141fa0d92bd2aba630fce96b92af1ef251
Misha <63599737+MichailNonProgramer@users.noreply.github.com>
2022-03-24 18:05:34 +0500

    Rename weight to width and realisation button custom size

--- src/shared/CustomButton.tsx
+++ src/shared/CustomButton.tsx
@@ -6,14 +6,14 @@ interface Props {
     readonly onClick?: (value: string) => void;
     readonly label: string;
     readonly classNameDiv?: string;
-    readonly weight?: number;
+    readonly width?: number;
     readonly height?: number;
 }
 
-const CustomButton = ({className, onClick, label, classNameDiv, weight, height}: Props) => {
+const CustomButton = ({className, onClick, label, classNameDiv, width, height}: Props) => {
     return (
         <div className={classNameDiv}>
-            <button className={className ?? 'custom_button'} onClick={() => onClick} >{label}</button>
+            <button style={{height: height, width: width}} className={className ?? 'custom_button'} onClick={() => onClick} >{label}</button>
         </div>
     )
 }
