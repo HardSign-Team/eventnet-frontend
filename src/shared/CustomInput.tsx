535ef4246ba1d4f97be2f530d872ab15b3cccddc
Misha <63599737+MichailNonProgramer@users.noreply.github.com>
2022-03-24 15:37:01 +0500

    Rename page to label

--- src/shared/CustomInput.tsx
+++ src/shared/CustomInput.tsx
@@ -7,14 +7,14 @@ interface Props {
     readonly value?: string;
     readonly onChange?: (value: string) => void;
     readonly placeholder?: string;
-    readonly page: string;
+    readonly label: string;
     readonly mask?: string;
 }
 
-const CustomInput = ({className, value, onChange, placeholder, page , mask}: Props) => {
+const CustomInput = ({className, value, onChange, placeholder, label , mask}: Props) => {
     return (
         <div>
-            <p >{page}</p>
+            <p >{label}</p>
             <Input mask={mask} className={className} value={value} placeholder={placeholder} onValueChange={() => onChange}/>
         </div>
     )
