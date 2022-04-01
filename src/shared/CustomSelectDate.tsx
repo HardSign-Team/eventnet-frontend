611a4482ab8e855819db3e7527908d911c4c1819
Misha <63599737+MichailNonProgramer@users.noreply.github.com>
2022-04-01 17:24:44 +0500

    Refactor code style

--- src/shared/CustomSelectDate.tsx
+++ src/shared/CustomSelectDate.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./CustomSelectDate.css";
 import { DatePicker, Gapped, Tooltip } from "@skbkontur/react-ui";
-import { ViewDateInputValidateChecks } from '@skbkontur/react-ui/components/DateInput/ViewDateInputValidateChecks';
+import { ViewDateInputValidateChecks } from "@skbkontur/react-ui/components/DateInput/ViewDateInputValidateChecks";
 
 interface PropsDateInput {
   readonly className?: string;
@@ -25,8 +25,8 @@ export const CustomSelectDate: React.FC<PropsDateInput> = ({
   const [error, setError] = useState(false);
   const [tooltip, setTooltip] = useState(false);
 
-  const minDate = '01.01.1900';
-  const maxDate = '01.01.2020';
+  const minDate = "01.01.1900";
+  const maxDate = "01.01.2020";
 
   const unvalidate = () => {
     setError(false);
@@ -34,17 +34,24 @@ export const CustomSelectDate: React.FC<PropsDateInput> = ({
   };
 
   const validate = () => {
-    const errorNew = !!date && !DatePicker.validate(date, { minDate: minDate, maxDate: maxDate });
+    const errorNew =
+      !!date &&
+      !DatePicker.validate(date, { minDate: minDate, maxDate: maxDate });
     setError(errorNew);
     setTooltip(errorNew);
   };
 
   let removeTooltip = () => setTooltip(false);
 
- return ( <Gapped gap={10} vertical >
-    <p className={classNameLabel ?? 'custom-input_label'}>{label}</p>
-    <Tooltip trigger={tooltip ? 'opened' : 'closed'} render={() => 'Невалидная дата'} onCloseClick={removeTooltip}>
-      <DatePicker
+  return (
+    <Gapped gap={7} vertical>
+      <p className={classNameLabel ?? "custom-input_label"}>{label}</p>
+      <Tooltip
+        trigger={tooltip ? "opened" : "closed"}
+        render={() => "Невалидная дата"}
+        onCloseClick={removeTooltip}
+      >
+        <DatePicker
           error={error}
           value={date}
           onValueChange={onChange}
@@ -54,9 +61,10 @@ export const CustomSelectDate: React.FC<PropsDateInput> = ({
           maxDate={maxDate}
           enableTodayLink
           width={width}
-          style={{height: height}}
+          style={{ height: height }}
           className={className}
-      />
-    </Tooltip>
-  </Gapped>);
+        />
+      </Tooltip>
+    </Gapped>
+  );
 };
