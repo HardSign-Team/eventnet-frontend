1a9218e7b67538e3e15c3301ec38a1ea1d35bb12
Misha <63599737+MichailNonProgramer@users.noreply.github.com>
2022-04-15 17:26:39 +0500

    Add user model for register and login

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,20 @@
-interface User {
+import { genders } from "../shared/GenderSelector/GenderSelector";
 
+interface User {}
+
+interface RegistrationUserInfo {
+  name: string;
+  email: string;
+  phone: string;
+  sex: genders;
+  password: string;
+  acceptedPassword: string;
+  born: string;
 }
 
-export default User;
+interface LoginUserInfo {
+  login: string;
+  password: string;
+}
+
+export type { User, RegistrationUserInfo, LoginUserInfo };
