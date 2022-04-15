0e9a4bf1cd2f65fabd49cbe978ea7d591714e20c
Misha <63599737+MichailNonProgramer@users.noreply.github.com>
2022-04-15 17:30:08 +0500

    Add validations

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -3,99 +3,104 @@ import Logo from "../shared/Logo";
 import "./Login.css";
 import CustomButton from "../shared/CustomButton/CustomButton";
 import { CustomInput } from "../shared/CustomInput/CustomInput";
-import { FormContainer } from '../shared/FormContainer/FormContainer';
-
-type userInfo = {
-  login: string;
-  password: string;
-};
-
-type errors = {
-  name: string;
-  password: string;
-};
+import { FormContainer } from "../shared/FormContainer/FormContainer";
+import { userInfo, loginRequest } from "../api/loginRequest";
+import {
+  text,
+  ValidationContainer,
+  ValidationWrapper,
+} from "@skbkontur/react-ui-validations";
+import { container, loginValidator, refContainer } from "../utils/Validators";
 
 const Login = () => {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState({ name: "", password: "" });
   const [errorName, setErrorName] = useState(false);
   const [errorPassword, setErrorPassword] = useState(false);
-  const [userInfo, setUserInfo] = useState({});
-  const login = () => {
-    console.log("login");
+
+  const login = async (): Promise<void> => {
+    if (!container) {
+      return;
+    }
+    if (await container.validate()) {
+      const user: userInfo = { login: userName, password: password };
+      console.log(user);
+      loginRequest(user).then((x) => console.log(x));
+    }
   };
 
-  const checkLogin = (userName: string, password: string) => {
-    const user: userInfo = { login: userName, password: password };
-    fetch(" https://localhost:7203/api/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(user),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.message);
-        setUserInfo(data);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
+  const checkLogin = () => {
+    login();
   };
+
+  const validator = loginValidator({
+    login: userName,
+    password: password,
+  });
 
   return (
     <div className="login">
       <div className="logo-login">
         <Logo className="logo_dess" width={200} height={200} />
       </div>
-      <FormContainer>
-        <CustomInput
-          label="Эл. почта или имя пользователя"
-          onChange={setUserName}
-          value={userName}
-        />
-        {(errorPassword || errorName) && (
-          <p className="Error">Неправильный логин или пароль</p>
-        )}
-        <CustomInput
-          label="Пароль"
-          onChange={setPassword}
-          type="password"
-          value={password}
-        />
-        <div className="password_handler">
-          <div className="label_helpers_source">
-            <a className="reset_password" href="../resetPassword">
-              Забыли пароль?
-            </a>
-            <div className="remember_password">
-              <input
-                id="checked"
-                type="checkbox"
-                className="checked_remember_password"
-              />
-              <label className="label_checked" htmlFor="checked">
-                Запомнить меня
-              </label>
+      <ValidationContainer ref={refContainer}>
+        <FormContainer>
+          <ValidationWrapper
+            validationInfo={validator.getNode((x) => x.login).get()}
+            renderMessage={text("right")}
+          >
+            <CustomInput
+              label="Эл. почта или имя пользователя"
+              onChange={setUserName}
+              value={userName}
+            />
+          </ValidationWrapper>
+          {(errorPassword || errorName) && (
+            <p className="Error">Неправильный логин или пароль</p>
+          )}
+          <ValidationWrapper
+            renderMessage={text("right")}
+            validationInfo={validator.getNode((x) => x.password).get()}
+          >
+            <CustomInput
+              label="Пароль"
+              onChange={setPassword}
+              type="password"
+              value={password}
+            />
+          </ValidationWrapper>
+          <div className="password_handler">
+            <div className="label_helpers_source">
+              <a className="reset_password" href="../resetPassword">
+                Забыли пароль?
+              </a>
+              <div className="remember_password">
+                <input
+                  id="checked"
+                  type="checkbox"
+                  className="checked_remember_password"
+                />
+                <label className="label_checked" htmlFor="checked">
+                  Запомнить меня
+                </label>
+              </div>
             </div>
+            <CustomButton
+              label="Войти"
+              className="login__button"
+              onClick={() => checkLogin()}
+              classNameDiv="label_login_button"
+              width={190}
+            />
           </div>
-          <CustomButton
-            label="Войти"
-            className="login__button"
-            onClick={() => checkLogin(userName, password)}
-            classNameDiv="label_login_button"
-            width={190}
-          />
-        </div>
-        <div className="change_to_registration">
-          <a className="not_acc">Нет аккаунта?</a>
-          <a className="change_rout_to_registration" href="/register">
-            Зарегистрироваться
-          </a>
-        </div>
-      </FormContainer>
+          <div className="change_to_registration">
+            <a className="not_acc">Нет аккаунта?</a>
+            <a className="change_rout_to_registration" href="/register">
+              Зарегистрироваться
+            </a>
+          </div>
+        </FormContainer>
+      </ValidationContainer>
     </div>
   );
 };
