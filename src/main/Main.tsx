43d89e6fc5aaaaa1d7e1208966e1af008a41877e
gODealOAple <maksim.math@gmail.com>
2022-05-06 17:17:24 +0500

    Add /api/events/search/name/{eventName} request

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,25 +1,28 @@
 import React, { useEffect } from "react";
 import YaMap from "./YandexMap/YaMap";
 import SideBar from "./SideBar/SideBar";
-import {buildRequestEventsParams, requestEvents} from "../api/events/getEvents";
+import {
+  buildRequestEventsParams,
+  requestEvents,
+} from "../api/events/getEvents";
 import { RequestEventDto } from "../dto/RequestEventDto";
 import { LocationFilterModel } from "../dto/LocationFilterModel";
 import { Location } from "../dto/Location";
 import { PageInfo } from "../dto/PageInfo";
 import { useLocation, useNavigate } from "react-router-dom";
 import globalStore from "../stores/GlobalStore";
-import {requestEvent} from "../api/events/getEvent";
+import { requestSearchByName } from "../api/events/searchByName";
 
 export default function Main() {
   const { search } = useLocation();
   const query = React.useMemo(() => new URLSearchParams(search), [search]);
   useEffect(() => {
     if (query.toString() !== "") {
-        requestEvents(query)
-            .then((r) => {
-                globalStore.eventStore.addEvents(r.events)
-            })
-            .catch((e: any) => console.log(e.message))
+      requestEvents(query)
+        .then((r) => {
+          globalStore.eventStore.addEvents(r.events);
+        })
+        .catch((e: any) => console.log(e.message));
     }
   });
   const navigate = useNavigate();
@@ -29,20 +32,24 @@ export default function Main() {
   };
 
   const handleClick = async () => {
-      // await request();
-      requestEvent("1D84109D-157E-4CAC-B989-5C23ABD0212D").then(r => console.log(r.event));
+    // await request();
+    //requestEvent("1D84109D-157E-4CAC-B989-5C23ABD0212D").then(r => console.log(r.event));
+    requestSearchByName("name", 10).then((r) => console.log(r.events));
   };
 
   const request = async () => {
-      const dto = new RequestEventDto(
-          {
-              radiusLocation: new LocationFilterModel(new Location(Math.random(), 0.6231),10),
-          },
-          new PageInfo(1, 10)
-      );
-      const params = buildRequestEventsParams(dto);
-      navigate(`/events?${params}`);
-  }
+    const dto = new RequestEventDto(
+      {
+        radiusLocation: new LocationFilterModel(
+          new Location(Math.random(), 0.6231),
+          10
+        ),
+      },
+      new PageInfo(1, 10)
+    );
+    const params = buildRequestEventsParams(dto);
+    navigate(`/events?${params}`);
+  };
 
   return (
     <div className="main-page">
