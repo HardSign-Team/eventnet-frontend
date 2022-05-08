611d94b43c68bfab5e5fef66ade13c9ef668a9a7
Misha <63599737+MichailNonProgramer@users.noreply.github.com>
2022-05-08 17:07:16 +0500

    Add request reset password

--- src/api/auth/resetPassword/index.ts
+++ src/api/auth/resetPassword/index.ts
@@ -1,1 +1,24 @@
-export {};
+async function resetPassword(
+  token: string,
+  mail: string,
+  newPassword: string,
+  confirmNewPassword: string
+) {
+  return fetch("http://localhost:5203/api/auth/password/reset", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      accept: "*/*",
+    },
+    body: JSON.stringify({
+      code: token,
+      email: mail,
+      newPassword: newPassword,
+      newPasswordConfirm: confirmNewPassword,
+    }),
+  })
+    .then((x) => x.json())
+    .catch((err) => console.error(err));
+}
+
+export { resetPassword };
