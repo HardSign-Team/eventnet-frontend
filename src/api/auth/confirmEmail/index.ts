ba80021891c1e13703708cb7efa2730bf8c3da0a
Misha <63599737+MichailNonProgramer@users.noreply.github.com>
2022-05-08 15:47:40 +0500

    Add request confirm mail register

--- src/api/auth/confirmEmail/index.ts
+++ src/api/auth/confirmEmail/index.ts
@@ -1,1 +1,17 @@
-export {};
+async function confirmEmail(userId: string, token: string) {
+  return fetch("http://localhost:5203/api/auth/email-confrimation-message", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+      Accept: "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify({
+      userId: userId,
+    }),
+  })
+    .then((x) => x.json())
+    .catch((err) => console.log(err));
+}
+
+export { confirmEmail };
--- src/completedRegister/index.tsx
+++ src/completedRegister/index.tsx
@@ -1,11 +1,18 @@
 import styles from "./index.module.scss";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { confirmEmail } from "../api/auth/confirmEmail";
 
 export const CompletedRegister: React.FC = () => {
   const { userId } = useParams();
   const { confirmKey } = useParams();
   const [isConfirmed, setIdConfirmed] = useState(true);
+  useEffect(() => {
+    if (userId !== undefined && confirmKey !== undefined)
+      confirmEmail(userId, confirmKey).then((x) => {
+        if (x.code === 200) setIdConfirmed(true);
+      });
+  }, [userId, confirmKey]);
   return (
     <div className={styles.completedRegister}>
       <div className={styles.content}>
