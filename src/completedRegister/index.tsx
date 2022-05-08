08308b7e4e1bebde6647cb86fdcdebfc3a09a029
Misha <63599737+MichailNonProgramer@users.noreply.github.com>
2022-05-08 16:41:54 +0500

    Refactor code

--- src/completedRegister/index.tsx
+++ src/completedRegister/index.tsx
@@ -2,6 +2,7 @@ import styles from "./index.module.scss";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { confirmEmail } from "../api/auth/confirmEmail";
+import { SUCCESS } from "../api/requestResponseCodes";
 
 export const CompletedRegister: React.FC = () => {
   const { userId } = useParams();
@@ -9,8 +10,8 @@ export const CompletedRegister: React.FC = () => {
   const [isConfirmed, setIdConfirmed] = useState(true);
   useEffect(() => {
     if (userId !== undefined && confirmKey !== undefined)
-      confirmEmail(userId, confirmKey).then((x) => {
-        if (x.code === 200) setIdConfirmed(true);
+      confirmEmail(userId, decodeURI(confirmKey)).then((x) => {
+        if (x.code === SUCCESS) setIdConfirmed(true);
       });
   }, [userId, confirmKey]);
   return (
