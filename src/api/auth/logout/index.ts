c34aeb0d6a5dc53daf59236f22fc2e56a4fb7572
Misha <63599737+MichailNonProgramer@users.noreply.github.com>
2022-05-15 21:08:49 +0500

    Add refresh token for logout

--- src/api/auth/logout/index.ts
+++ src/api/auth/logout/index.ts
@@ -1,7 +1,20 @@
 import { BASE_ROUTE, HTTP_METHODS, STATUS_CODES } from "../../utils";
+import { refreshToken } from "../refreshToken";
+import globalStore from "../../../stores/GlobalStore";
 
 async function logout(token: string) {
-  const response = await fetch(BASE_ROUTE + "/api/auth/logout", {
+  const response = await request(token);
+  if (response.statusText === "Unauthorized") {
+    await refreshToken(globalStore.userStore.refreshToken, token);
+    return (
+      (await request(globalStore.userStore.accessToken)).status ===
+      STATUS_CODES.OK
+    );
+  } else return response.status === STATUS_CODES.OK;
+}
+
+async function request(token: string) {
+  return await fetch(BASE_ROUTE + "/api/auth/logout", {
     method: HTTP_METHODS.POST,
     headers: {
       "Content-Type": "application/json;charset=utf-8",
@@ -9,7 +22,6 @@ async function logout(token: string) {
       Authorization: "Bearer " + token,
     },
   });
-  return response.status === STATUS_CODES.OK;
 }
 
 export { logout as logoutRequest };
