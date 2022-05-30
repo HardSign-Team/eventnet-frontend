e55a43544581f586194526764b6455c7566cbf1e
Banan-dot <semke.danya@mail.ru>
2022-05-30 13:10:34 +0500

    Fix date bugs

--- src/pages/events/YandexMap/Circles/Circles.tsx
+++ src/pages/events/YandexMap/Circles/Circles.tsx
@@ -8,16 +8,31 @@ import { requestEvent } from "../../../../api/events/getEvent";
 import { Coordinates } from "../../../../models/Coordinates";
 import { guid } from "../../../../viewModels/Guid";
 import globalStore from "../../../../stores/GlobalStore";
-import { eventViewModelToEvent } from "../../../../utils/convertHelper";
+import {
+  eventViewModelToEvent,
+  locationToCoordinates,
+} from "../../../../utils/convertHelper";
+
 const CIRCLE_RADIUS = 5;
 const CIRCLE_COLOR = "#008D8E";
-const circleOptions = {
-  fill: true,
-  fillColor: CIRCLE_COLOR,
-  strokeColor: CIRCLE_COLOR,
-  opacity: 1,
-  strokeOpacity: 1,
-  strokeWidth: 15,
+
+const getEvent = async (eventId: guid) => {
+  const evm = await requestEvent(eventId);
+  return eventViewModelToEvent(evm.event);
+};
+
+const getEventId = (events: EventLocationViewModel[], coords: Coordinates) => {
+  let eventId = "";
+  let min = 10000;
+  events.forEach((e) => {
+    const dx = Math.abs(coords[0] - e.location.latitude);
+    const dy = Math.abs(coords[1] - e.location.longitude);
+    if (dx * dx + dy * dy < min) {
+      eventId = e.id;
+      min = dx * dx + dy * dy;
+    }
+  });
+  return eventId;
 };
 
 type Props = {
@@ -26,30 +41,19 @@ type Props = {
 
 const Circles = ({ events }: Props) => {
   const circles: Array<JSX.Element> = [];
-
-  const getEvent = async (eventId: guid) => {
-    const evm = await requestEvent(eventId);
-    return eventViewModelToEvent(evm.event);
-  };
-
-  const getEventId = (coords: Coordinates) => {
-    let eventId = "";
-    let min = 10000;
-    events.forEach((e) => {
-      const dx = Math.abs(coords[0] - e.location.latitude);
-      const dy = Math.abs(coords[1] - e.location.longitude);
-      if (dx * dx + dy * dy < min) {
-        eventId = e.id;
-        min = dx * dx + dy * dy;
-      }
-    });
-    return eventId;
+  const circleOptions = {
+    fill: true,
+    fillColor: CIRCLE_COLOR,
+    strokeColor: CIRCLE_COLOR,
+    opacity: 1,
+    strokeOpacity: 1,
+    strokeWidth: 15,
   };
 
   const onMouseEnter = async (c: any) => {
     const map = c.get("map");
     const coords = c.get("coords");
-    const eventId = getEventId(coords);
+    const eventId = getEventId(events, coords);
     const event = await getEvent(eventId);
 
     globalStore.eventStore.addEvent(event);
@@ -75,9 +79,9 @@ const Circles = ({ events }: Props) => {
   events.forEach((e) => {
     circles.push(
       <Circle
-        geometry={[[e.location.latitude, e.location.longitude], CIRCLE_RADIUS]}
-        options={circleOptions}
         key={e.id}
+        geometry={[locationToCoordinates(e.location), CIRCLE_RADIUS]}
+        options={circleOptions}
         onMouseLeave={onMouseLeave}
         onMouseEnter={onMouseEnter}
         modules={["geoObject.addon.balloon", "geoObject.addon.hint"]}
